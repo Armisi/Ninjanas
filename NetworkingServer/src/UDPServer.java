050cefb2a1f6f4ccd14c2afb91d4c9ae852d859b
Armisi <armisimail@gmail.com>
2015-08-22 19:14:41 +0300

    Network
    Server

--- NetworkingServer/src/UDPServer.java
+++ NetworkingServer/src/UDPServer.java
@@ -4,18 +4,20 @@ import java.net.*;
 class UDPServer {
 	public static void main(String args[]) throws Exception {
 		DatagramSocket serverSocket = new DatagramSocket(9876);
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[64];
+		byte[] sendData = new byte[64];
 		while (true) {
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
 			String sentence = new String(receivePacket.getData());
-			System.out.println("RECEIVED: " + sentence);
+			//if(!sentence.equals("asdasd")){
+				System.out.println("RECEIVED: " + sentence);
+			//}
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			String capitalizedSentence = sentence.toUpperCase();
 			sendData = capitalizedSentence.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 15232);
 			serverSocket.send(sendPacket);
 		}
 	}
